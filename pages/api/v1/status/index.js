25e0891e10138076947ab9e36d08ab906168701e
Leandro Bonroa <60291393+LBonora@users.noreply.github.com>
2024-12-17 20:32:51 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,7 @@
-function status(request, response) {
-  response.status(200).send({ OK: "OK" });
+import database from "../../../../infra/database";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  response.status(200).send(result.rows[0]);
 }
 
 export default status;
