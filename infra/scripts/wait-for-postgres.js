9c1694e212c57565761b222e193fff0aa5a5b799
lbonora <leandro.bonora@gmail.com>
2025-02-21 17:39:26 -0300

    style: make `wait-for-postgres.js` juicier

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,16 +1,23 @@
 const { exec } = require("node:child_process");
 
-function checkPostgres() {
+let count = 0;
+//const clock = ["🌑", "🌘", "🌗", "🌖", "🌕", "🌔", "🌓", "🌒"];
+const clock = ["📨   📪", " 📨  📪", "  📨 📪", "   📨📭", "     📬"];
+
+function checkPostgres(timeout = 50) {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
-      checkPostgres();
+      process.stdout.write(
+        `\r${clock[count % clock.length]} Waiting for PostgreSQL accept connections...`,
+      );
+      count++;
+
+      setTimeout(checkPostgres, timeout);
       return;
     }
-    console.log("\n🟢 Postgres está aceitando conexões\n");
+    console.log("\n     📫 PostgreSQL is ready and accepting connections!\n");
   }
 }
-process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
 checkPostgres();
